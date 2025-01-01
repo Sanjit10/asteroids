f15d8c672f3f4eb82c130b6ba521409af846ed37
Sanjit Khanal <sanjit.khanal@nimble-cr.com>
2025-01-01 20:03:20 +0545

    Modify player to use spirit groups for easier update

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        [item.draw(screen) for item in drawable]
+        [item.update(dt) for item in updatable]
+
         pygame.display.flip()
         delta_time = clock.tick(60)
         dt = delta_time/1000
